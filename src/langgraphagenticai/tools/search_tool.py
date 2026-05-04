c2949537f7929234badae9d94219dcc5c799969f
abhidharsh <abhidharsh1999@gmail.com>
2026-05-04 19:22:03 +0530

    chatbot with tools is completed

---
+++ src/langgraphagenticai/tools/search_tool.py
@@ -0,0 +1,18 @@
+# from langchain_community.tools import TavilySearchResults
+from langgraph.prebuilt import ToolNode
+from langchain_tavily import TavilySearch
+
+
+def get_tools():
+    """
+    Return the list of tools to be used in the chatbot
+    """
+    tools = [TavilySearch(max_results=2)]
+    return tools
+
+
+def create_tool_node(tools):
+    """
+    Creates and returns a tool node for the graph
+    """
+    return ToolNode(tools=tools)
